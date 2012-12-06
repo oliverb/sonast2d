cffa86241ba87dee17e6ec3a47c3f0b1f887239e
Oliver Behm <oliverbehm@gmail.com>
2012-12-06 22:07:04 +0100

    File that provides flagfield macros for python and weave

--- sonast2d/nast2d/cmacros.py
+++ sonast2d/nast2d/cmacros.py
@@ -1,10 +1,22 @@
-flags = [("OC", 0x00),
-         ("FC", 0x10),
-         ("B_N", 0x01),
-         ("B_S", 0x02),
-         ("B_W", 0x04),
-         ("B_E", 0x08),
-         ("B_NW", 0x05),
-         ("B_SW", 0x06),
-         ("B_SE", 0x0A),
-         ("B_NE", 0x09)]
+
+OC = 0x00
+FC = 0x10
+B_N = 0x01
+B_S = 0x02
+B_W = 0x04
+B_E = 0x08
+B_NW = 0x05
+B_SW = 0x06
+B_SE = 0x0A
+B_NE = 0x09
+
+flag_dict = [("OC", OC),
+         ("FC", FC),
+         ("B_N", B_N),
+         ("B_S", B_S),
+         ("B_W", B_W),
+         ("B_E", B_E),
+         ("B_NW", B_NW),
+         ("B_SW", B_SW),
+         ("B_SE", B_SE),
+         ("B_NE", B_NE)]
