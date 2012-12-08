03175847a79f6e2235e9092a6076b3b28c64c1fa
Oliver Behm <oliverbehm@gmail.com>
2012-12-08 22:54:16 +0100

    NaSt2D Updated driven cavity description

--- sonast2d/run_drivencavity.py
+++ sonast2d/run_drivencavity.py
@@ -1,4 +1,24 @@
 import cProfile
+import numpy
+from math import pi
 from nast2d import solver
 
-cProfile.run('solver.solve()')
+class DrivenCavity:
+    def __init__(self, imax=64, jmax=64):
+        self.xlength = 1.0
+        self.ylength = 1.0
+
+        self.imax = imax
+        self.jmax = jmax 
+
+        self.Re = 100.0
+        self.omega = 2.0-2*pi*self.xlength/self.imax
+        self.tau = 0.5
+        self.alpha = 0.9
+        self.eps = 1E-7
+        self.max_it = 200
+
+        self.geo = numpy.zeros((imax+2,jmax+2), dtype=numpy.int32)
+
+# cProfile.run('solver.solve(solver.DrivenCavity())')
+solver.solve(DrivenCavity())
