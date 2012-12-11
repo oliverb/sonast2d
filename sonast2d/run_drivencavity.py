7308c32f8bc97f11faf1d77e6420b66045f9b257
Oliver Behm <oliverbehm@gmail.com>
2012-12-11 23:57:30 +0100

    NaSt2D updated driven cavity example

--- sonast2d/run_drivencavity.py
+++ sonast2d/run_drivencavity.py
@@ -7,19 +7,21 @@ class DrivenCavity:
     def __init__(self, imax=64, jmax=64):
         self.xlength = 1.0
         self.ylength = 1.0
-        self.t_end = 5.0
+        self.t_end = 20.0
 
         self.imax = imax
         self.jmax = jmax 
 
-        self.Re = 100.0
+        self.Re = 50.0
         self.omega = 2.0-2*pi*self.xlength/self.imax
         self.tau = 0.5
         self.alpha = 0.9
         self.eps = 1E-7
-        self.max_it = 200
+        self.max_it = 2000
+
+        self.boundary = {'scenario':'cavity'}
 
         self.geo = numpy.zeros((imax+2,jmax+2), dtype=numpy.int32)
 
 # cProfile.run('solver.solve(solver.DrivenCavity())')
-solver.solve(DrivenCavity())
+solver.solve(DrivenCavity(128,128))
