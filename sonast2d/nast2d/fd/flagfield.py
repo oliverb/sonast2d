0ccedc18e2155e53cd22665aee12995b6c08d4fd
Oliver Behm <oliverbehm@gmail.com>
2012-12-13 17:31:45 +0100

    nast2d new flagfield generation code

--- sonast2d/nast2d/fd/flagfield.py
+++ sonast2d/nast2d/fd/flagfield.py
@@ -40,6 +40,52 @@ def generate_flagfield(geo):
         return_val = flc;
     """
 
-    nfc = weave.inline(code, ['flags', 'geo'], define_macros=cmacros.flag_dict)
+    code2 = """
+        #line 44 "flagfield.py"
+        int imax = Ngeo[0]-2;
+        int jmax = Ngeo[1]-2;
+
+        /* border cells are always obstacles */
+        for(int i=1; i<=imax; i++) {
+            FLAGS2(i,0) = OC + (1-GEO2(i,1))*B_N;
+            FLAGS2(i,jmax+1) = OC + (1-GEO2(i,jmax))*B_S;
+        }
+
+        for(int j=1; j<=jmax; j++) {
+            FLAGS2(0,j) = OC + (1-GEO2(1,j))*B_E;
+            FLAGS2(imax+1,j) = OC + (1-GEO2(imax,j))*B_W;
+        }
+
+        /* first pass */
+        int flc = 0; // count fluid cells
+        for(int i=1; i<=imax; i++) {
+            for(int j=1; j<=jmax; j++) {
+                if(GEO2(i,j)) {
+                    FLAGS2(i,j) = OC;
+                } else {
+                    FLAGS2(i,j) = FC;
+                    flc++;
+                }
+            }
+        }
+
+        /* second pass to update boundary information */
+        int flc = 0; // count fluid cells
+        for(int i=1; i<=imax; i++) {
+            for(int j=1; j<=jmax; j++) {
+                if(!(FLAG2(i,j)&FC)) {
+                    FLAG2(i,j) += (FLAG2(i-1,j)&FC)*B_W
+                                 +(FLAG2(i+1,j)&FC)*B_E
+                                 +(FLAG2(i,j+1)&FC)*B_N
+                                 +(FLAG2(i,j-1)&FC)*B_S;
+                }
+                /* TODO check flagfield for illegal obstacle cells */
+            }
+        }
+
+        return_val = flc;
+    """
+
+    nfc = weave.inline(code2, ['flags', 'geo'], define_macros=cmacros.flag_dict)
 
     return nfc, flags
