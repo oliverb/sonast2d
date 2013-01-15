fbcb3255acd2c37c469862446cc36970c2e02d0a
Oliver Behm <oliverbehm@gmail.com>
2013-01-15 16:26:02 +0100

    nast2d pretty print for flagfield

--- sonast2d/nast2d/output/vtk.py
+++ sonast2d/nast2d/output/vtk.py
@@ -1,5 +1,41 @@
 from .. import cmacros
 
+def output_flag(flags, dx, dy, n, name="default_p"):
+    m1, m2 = flags.shape
+    imax = m1-2
+    jmax = m2-2
+
+    filename = "%s_%04d.vtk" % (name, n, )
+    with open(filename, "w") as f:
+        f.write("# vtk DataFile Version 3.0\n")
+        f.write("Scalar Field\n")
+        f.write("ASCII\n")
+        f.write("DATASET RECTILINEAR_GRID\n")
+
+        f.write("DIMENSIONS %d %d 1\n" % (imax, jmax, ))
+
+        f.write("X_COORDINATES %d double\n" % (imax, ))
+        for i in range(1, imax+1):
+            f.write("%f " % (dx*(i-1)))
+        f.write("\n")
+
+        f.write("Y_COORDINATES %d double\n" % (jmax, ))
+        for j in range(1, jmax+1):
+            f.write("%f " % (dy*(j-1)))
+        f.write("\n")
+
+        f.write("Z_COORDINATES 1 double\n0.0\n")
+
+        f.write("POINT_DATA %d\n" % (imax*jmax, ))
+        f.write("Scalars Pressure double\n")
+        f.write("LOOKUP_TABLE default\n")
+
+        for j in range(1, jmax+1):
+            for i in range(1, imax+1):
+                if flags[i,j]&cmacros.FC:
+                    f.write("1.0\n")
+                else:
+                    f.write("0.0\n")
 def output_scalar(p, flags, dx, dy, n, name="default_p"):
     m1, m2 = p.shape
     imax = m1-2
